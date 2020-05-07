857bf6898b76670f4a745061479819216cef013f
João Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-05-07 18:15:32 -0300

    Create datetime.py

---
+++ src/Initial/datetime.py
@@ -0,0 +1,10 @@
+#import the library 
+import datetime
+
+#Def the x as a variable and catching the datetime now(today)
+x = datetime.datetime.now()
+
+#print the day, year and the name of the day
+print(x.day)
+print(x.year)
+print(x.strftime("%A"))
