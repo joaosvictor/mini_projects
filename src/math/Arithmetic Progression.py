6df5c4cfb9f49b72d887c5fb43014570df77595a
João Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-05-08 15:08:17 -0300

    Create Arithmetic Progression.py

---
+++ src/math/Arithmetic Progression.py
@@ -0,0 +1,36 @@
+#by João Victor : joaosantoos@aol.com
+
+from time import sleep
+#import the library
+
+print('---- Super Arithmetic Progression ----')
+
+#Setting up the variables
+term1 = int(input('What the first term ? '))
+cmdifference = int(input('Common difference ? '))
+amount = int(10)
+plus = int(10)
+suM = int(0)
+counter = (0)
+
+#loop while to do the process
+while counter < amount:
+    counter = counter + 1
+    for c in range(counter, amount +1 ):
+        if c == 0:
+            suM = term1
+        else:
+            suM = suM + cmdifference
+        print(suM - cmdifference, end=' => ')
+
+    #putting the continnue of AP
+    plus = int(input('More terms ? '))
+    amount = counter + plus
+    if plus == 0:
+        amount = counter + plus
+
+#finishing the code 
+print('Finishing...')
+sleep(2)
+print('==================================')
+print('Program ended! To the next! ')
