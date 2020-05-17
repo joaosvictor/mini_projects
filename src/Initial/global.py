158f0f1bc9f69d4da22192e815dd127fe88300c9
Victor Santos <48931503+joaovictorsantos@users.noreply.github.com>
2020-05-16 22:48:17 -0300

    Create global.py

---
+++ src/Initial/global.py
@@ -0,0 +1,42 @@
+#Global tuturial 
+
+x = 1 #global variable
+
+def add():
+	print(x)
+add()	
+
+#############################
+
+x = 1 #global variable 
+
+def add():
+	x = x + 2 #increment x by 2
+	print(x)
+add()
+
+#############################	
+
+x = 0 #global variable
+
+def add():
+	global x 
+	x = x + 2 #increment by 2
+	print('inside add():', x)
+add()
+print('In main:', x)
+
+#############################
+# Using a global variable in nested function
+def too():
+	x = 20
+
+	def exp():
+		global x
+		x = 25
+	print('Before calling bar: ', x)
+	print('Calling bar now')
+	exp()
+	print('After calling bar: ', x)
+too()
+print('x in main: ', x)
