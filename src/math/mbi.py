e3fae46ae280c2de498bf038e6bb5d37aa1e4df0
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-05-18 14:34:33 -0300

    Create mbi.py

---
+++ src/math/mbi.py
@@ -0,0 +1,11 @@
+name  = str(input('Your name: '))
+height_m = float(input('Put your height: '))
+weight_kg = float(input('Put your weight: '))
+
+bmi = weight_kg / (height_m ** 2)
+print("{} your bmi is: {}".format(name,bmi))
+if bmi < 25:
+	print('you are not overweight')
+else:
+    print('is overweight')
+    
