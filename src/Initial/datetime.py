a2a91067f823fd37c974f7f5ec1b5ad9b97f438c
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-06-29 09:56:53 -0300

    Update datetime.py

--- src/Initial/datetime.py
+++ src/Initial/datetime.py
@@ -4,7 +4,7 @@ import datetime
 #Def the x as a variable and catching the datetime now(today)
 x = datetime.datetime.now()
 
-#print the day, year and the name of the day
+#print the day, year and the name of the day;
 print(x.day)
 print(x.year)
 print(x.strftime("%A"))
