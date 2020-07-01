849426c23f76e3ae3749acac1510d6d5248ce4b0
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-06-30 22:21:25 -0300

    Create hashTable.py

---
+++ Algorithms/hashTable.py
@@ -0,0 +1,13 @@
+#check voter 
+#hash table will call the dictionary
+voted = {} #here are the hash table = "{}"
+def check_voter(name):
+  if voted.get(name):
+    print("kick them out!")
+  else:
+    voted[name] = True
+    print("let them vote!")
+
+check_voter("Victor")
+check_voter("Ana")
+check_voter("Thiago")
