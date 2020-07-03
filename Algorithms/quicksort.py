ee3f3c4c915b7e68069dbc22335374ab459fd17e
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-07-03 10:49:35 -0300

    Update quicksort.py

--- Algorithms/quicksort.py
+++ Algorithms/quicksort.py
@@ -1,10 +1,14 @@
 def quicksort(array):
-  if len(array) < 2:
+  #in quicksort you must have a pivot in the code
+  #the pivot will take a random No in the list/array, usually the mid number 
+  if len(array) < 2:# if <2 return, 'cause think if the element is the first No ? you'll waste time finding in the mid. 
     return array
   else:
-    pivot = array[0]
+    #the driver code below make the whole proccess
+    pivot = array[0]#remember this '[0]' is the index of the array.
     less = [i for i in array[1:] if i <= pivot]
     greater = [i for i in array[1:] if i > pivot]
     return quicksort(less) + [pivot] + quicksort(greater)
 
-print(quicksort([10, 5, 2, 3]))
+print(quicksort([10, 5, 2, 3]))#->2,3,5,10
+#if you use this code for a cp you can use in that way print(quicksort(list(int,input().split())))
