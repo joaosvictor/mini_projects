5c4b7dd327587ba305598694b85cfff7f50200f0
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-07-03 10:50:48 -0300

    Update generatorfloat.py

--- src/math/generator/generatorfloat.py
+++ src/math/generator/generatorfloat.py
@@ -1,6 +1,6 @@
 from random import seed
 from random import random
 
-for _ in range(200):
-      value = random(1, 1500)
+for _ in range(200):#you can extend this number for a 500 or 1000
+      value = random(1, 1500)#this will generate only 1500 No
       print(value)
