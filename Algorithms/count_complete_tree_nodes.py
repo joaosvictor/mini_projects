f45e3adf8678e505c86cf8828017d3d2b39c753f
João Victor <joaosantoos@aol.com>
2020-08-27 00:19:28 -0300

    Update and rename count complete tree nodes.py to count_complete_tree_nodes.py

--- Algorithms/count_complete_tree_nodes.py
+++ Algorithms/count_complete_tree_nodes.py
@@ -53,7 +53,6 @@ class Solution:
 # Space: O(1)
 
 # Given a complete binary tree, count the number of nodes.
-#
 # In a complete binary tree every level, except possibly the last,
 # is completely filled, and all nodes in the last level are as far
 # left as possible. It can have between 1 and 2h nodes inclusive at
