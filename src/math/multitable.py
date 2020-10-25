d5a0bc2486e9de09f01fec4465f93680bab36d08
João Victor <joaosantoos@aol.com>
2020-10-25 13:14:39 -0300

    Update multitable.py

--- src/math/multitable.py
+++ src/math/multitable.py
@@ -5,6 +5,6 @@ print('*' * 18)
 print('Multiplication table of {}'.format(value))  
 print('*' * 18)  
 while(aux <= 10):  
-  print('{0} X {1} = {2}'.format(aux, value, (aux * value)))  #this will x your value number times the rest of the multitable.
+  print('{0} X {1} = {2}'.format(aux, value, (aux * value)))  # this will x your value number times the rest of the multitable
   aux = aux + 1 
   #
