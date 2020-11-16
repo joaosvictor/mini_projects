27c912ea7c536cb10d1ec7b51214fc369e1d6f60
João Victor <joaosantoos@aol.com>
2020-11-16 19:01:50 -0300

    feature added

--- cryptography/window.py
+++ cryptography/window.py
@@ -5,16 +5,14 @@ import os
 import random
 import string 
 import sys
-import numpy as np 
 
 class Convert(object):
     
-    frame = np.array([-500,-500])
     root = Tk()
-    root.geometry(frame)
+    root.geometry("500x500")
     root.title("Cryptography")
 
-    def take_input(chars = string.ascii_uppercase + string.digts + string.punctuation):
+    def take_input(chars = string.ascii_uppercase + string.digits + string.punctuation):
         INPUT = inputtxt.get("1.0", "end-1c")
         convert_num_str = f'{INPUT}'
         sys.stdout.write("\033[F")
@@ -34,7 +32,12 @@ class Convert(object):
         """
         # part to transform it 
         transformation  = ''.join(random.choice(chars) for i in range(len(INPUT)*10))
-
+        print(transformation)
+    l.pack()
+    inputtxt.pack()    
+    Display.pack()    
+    mainloop()
+    take_input()
 
 if __name__ == '__main__':
     Convert()
