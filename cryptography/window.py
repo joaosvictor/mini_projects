527cc033831ff31d5c895c01e87d2551df947dac
João Victor <joaosantoos@aol.com>
2020-11-17 18:08:10 -0300

    add comment

--- cryptography/window.py
+++ cryptography/window.py
@@ -8,10 +8,9 @@ import string
 import sys
 
 class Convert(object):
-    frame = np.array([500,500])
+
     root = Tk()
-    root.geometry(frame)
-   #root.geometry("500x500")
+    root.geometry("500x500")
     root.title("Cryptography")
 
     def take_input(chars = string.ascii_uppercase + string.digits + string.punctuation):
