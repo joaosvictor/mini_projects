178b3ad621510efae4946523c8965a8a13a6d8e0
João Victor <joaosantoos@aol.com>
2020-11-19 19:57:00 -0300

    add new todo

--- cryptography/td.py
+++ cryptography/td.py
@@ -1,4 +1,5 @@
 # TODO: try copy the cryptography text
+# TODO: when closed don't put a none sentence(fix it with a loop) 
 # TODO: optimize the window with numpy
 # TODO: add a high size to the window
 # TODO: add a terminal version in the dir
