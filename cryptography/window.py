3e15a1dc02654813f47adc4ef5e84ec73cf51101
João Victor <joaosantoos@aol.com>
2020-11-19 18:15:23 -0300

    no while

--- cryptography/window.py
+++ cryptography/window.py
@@ -3,18 +3,25 @@
 import os 
 import sys
 import random
+import string
 import PySimpleGUI as sg 
 
 class Converttxt(object):
-    def process():
+    def process(chars = string.ascii_uppercase + string.digits + string.punctuation):
 
         # window color theme
         sg.theme('Black')
 
+
         # inside your window 
         layout = [ [sg.Text('Cryptography your text now! Cr+.')],
-                   [sg.Text('Please, enter your text: '), sg.InputText()],
-                   [sg.Button('Convert'), sg.Button('Cancel')]]
+                   [sg.Text('Please, enter your text: '), sg.InputText(key = '-IN-')],
+                   [sg.Submit(), sg.Cancel()]]
+ 
+        '''
+        convert_num_str = f'{layout}'
+        transformation = ''.join(random.choice(chars) for i in range(len(convert_num_str) * 10))
+        '''
 
         # create window
         window = sg.Window('Cr+', layout)
@@ -24,7 +31,7 @@ class Converttxt(object):
             event, values = window.read()
             if event == sg.WIN_CLOSED or event == 'Cancel':
                 break
-            print('You entered ', values[0])
+            print('Your cryptography text:  ', values[0])
 
         window.close()
     process()
