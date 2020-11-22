70e2d370c8d68f123cf925b243cba0ba24919c8f
João Victor <joaosantoos@aol.com>
2020-11-22 15:08:43 -0300

    refresh td

--- cryptography/td.py
+++ cryptography/td.py
@@ -1,6 +1,5 @@
 # TODO: try copy the cryptography text
 # TODO: when closed don't put a none sentence(fix it with a loop) 
-# TODO: add a shell script of run the code
 # TODO: optimize the window with numpy
 # TODO: add a high size to the window
 # TODO: add a terminal version in the dir
