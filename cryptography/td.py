8fd21f36f9b6a91f7e9d4e2a2f7883537190ae97
João Victor <joaosantoos@aol.com>
2020-11-23 00:04:05 -0300

    add new todo

--- cryptography/td.py
+++ cryptography/td.py
@@ -1,5 +1,7 @@
 # TODO: try copy the cryptography text
 # TODO: when closed don't put a none sentence(fix it with a loop) 
+# TODO: https://stackoverflow.com/questions/43925624/fatest-method-to-dump-numpy-array-into-string
+# TODO: add time execution
 # TODO: optimize the window with numpy
 # TODO: add a high size to the window
 # TODO: add a terminal version in the dir
