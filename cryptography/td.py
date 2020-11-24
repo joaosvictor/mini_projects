a1d53f7c925dd916fedb321b37168760d47f30c9
João Victor <joaosantoos@aol.com>
2020-11-24 09:58:13 -0300

    refresh the td

--- cryptography/td.py
+++ cryptography/td.py
@@ -1,3 +1,2 @@
 # TODO: when closed don't put a none sentence(fix it with a loop) 
-# TODO: https://stackoverflow.com/questions/43925624/fatest-method-to-dump-numpy-array-into-string
 # TODO: optimize the window with numpy
