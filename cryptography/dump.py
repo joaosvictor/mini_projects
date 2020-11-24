6f15cdf27f06686b12cddf28a28c270368173d18
João Victor <joaosantoos@aol.com>
2020-11-24 10:09:58 -0300

    add a dump model

---
+++ cryptography/dump.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# data. dump model.
+import io
+import zlib
+import numpy as np
+
+code = io.BytesIO()
+arr = np.random.rand(100,100)
+np.save(code, arr)
+compressed = zlib.compress(code.getvalue())
+
+prt = np.load(io.BytesIO(zlib.decompress(compressed)))
+
+print(prt)
