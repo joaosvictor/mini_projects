f43e744e22f4659a1222b55e935940571d761af9
João Victor <joaosantoos@aol.com>
2020-11-24 09:42:40 -0300

    r

--- cryptography/td.py
+++ cryptography/td.py
@@ -1,4 +1,3 @@
-# TODO: try copy the cryptography text
 # TODO: when closed don't put a none sentence(fix it with a loop) 
 # TODO: https://stackoverflow.com/questions/43925624/fatest-method-to-dump-numpy-array-into-string
 # TODO: add time execution
