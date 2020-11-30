aa9630b662e1b310ae3a0fa621865296495a318a
João Victor <joaosantoos@aol.com>
2020-11-30 18:39:55 -0300

    indentation fixed and added link

--- Algorithms/bellmanFord.cc
+++ Algorithms/bellmanFord.cc
@@ -1,17 +1,19 @@
+// bellman ford algorithm
+// https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm
+
 #include <iostream>
 using namespace std;
 
 int main() {
-
-	// the implementation:
+    // the implementation:
     for (int i = 1; i <= n; i++) distance[i] = INF;
     distance[x] = 0;
     for (int i = 1; i <= n-1; i++){
-		for (auto e:edges){
-			int a, b, w,
-			tie(a,b,w) = e;
-			distance[b] = min(distance[b], distance[a]+w); 
+       	for (auto e:edges){
+       		int a, b, w,
+       		tie(a,b,w) = e;
+       		distance[b] = min(distance[b], distance[a]+w); 
 		
-			}
-		}
+    		}
+       	}
 };
