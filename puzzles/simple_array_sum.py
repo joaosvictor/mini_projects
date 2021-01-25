e51605883947802df6a72381e852b6778b507e3b
João Victor <joaosantoos@aol.com>
2021-01-25 17:11:04 -0300

    removing '#'

--- puzzles/simple_array_sum.py
+++ puzzles/simple_array_sum.py
@@ -4,13 +4,13 @@
 import os
 import sys
 
-#
+
 # Complete the simpleArraySum function below.
-#
+
 def simpleArraySum(ar):
-    #
+    
     # Write your code here.
-    #
+    
     sum =0
     for i in range(len(ar)) :
         sum+=ar[i]
