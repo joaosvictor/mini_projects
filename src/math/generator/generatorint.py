d9ced945d03c694103b3540c518a4e244c11c765
João Victor <joaosantoos@aol.com>
2021-02-07 11:04:50 -0300

    more numbers

--- src/math/generator/generatorint.py
+++ src/math/generator/generatorint.py
@@ -1,6 +1,6 @@
 from random import seed
 from random import randint
 
-for _ in range(200): 
-      value = randint(1, 1500)
+for i in range(200): 
+      value = randint(1, 150000)
       print(value)
