ddf204b2bc735e4a1f86076badbe009677e77527
João Victor <joaosantoos@aol.com>
2021-02-10 09:48:13 -0300

    introduction

---
+++ sql/session01.sql
@@ -0,0 +1,12 @@
+-- here is the code without creating the database, 'cause sqlive dt need
+
+create table tb_client(
+        client_id int primary key,
+        client_name varchar(155),
+        client_phone varchar(50),
+        client_email varchar(240)
+);
+
+select * from tb_client;
+
+insert into tb_client values(1,'João Victor','(11)961878349','joao@gmail.com');
