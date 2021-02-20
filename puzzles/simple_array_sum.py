e30e5eca48caf4e39f30b2c2ac12ac034d1b3c33
João Victor <joaosantoos@aol.com>
2021-02-20 10:45:05 -0300

    output invalid

--- puzzles/simple_array_sum.py
+++ puzzles/simple_array_sum.py
@@ -15,7 +15,7 @@ def simpleArraySum(ar):
     for i in range(len(ar)) :
         sum+=ar[i]
     return sum
-        
+'''        
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
@@ -28,3 +28,4 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+'''
