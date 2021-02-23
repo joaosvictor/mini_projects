c6abdd54990fba02c7f85e5ae336c3c1425e4b64
João Victor <joaosantoos@aol.com>
2021-02-23 10:19:59 -0300

    puzzle

---
+++ sql/session02.sql
@@ -0,0 +1,5 @@
+-- a hackerrank problem 
+-- https://www.hackerrank.com/challenges/revising-the-select-query/problem?h_r=profile
+
+select * from city
+where countrycode = 'USA' and population > 100000
