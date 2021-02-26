adfbacdcd7704553772659f63fad56c669daa3e3
João Victor <joaosantoos@aol.com>
2021-02-26 10:48:14 -0300

    solve puzzle

---
+++ sql/session03.sql
@@ -0,0 +1,12 @@
+-- a hackerrank problem
+-- https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+
+-- return differents values
+select distinct city
+from station
+
+-- mod() return the remainder of values
+where mod(id, 2) = 0
+
+-- order by is used to sort the result
+order by city;
