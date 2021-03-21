aef7da592c0b03b6bada23d1b7cd3a509e85f7b7
João Victor <joaosantoos@aol.com>
2021-03-21 12:27:07 -0300

    find the height/width of a image

---
+++ src/discoverWidHei.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+
+# get the image path
+image = ""
+img = Image.open(image)
+
+width = img.Width
+height = img.Height
+
+# print the result
+print('the height: ',height)
+print('the width: ',width)
